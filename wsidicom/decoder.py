daa79fb32a36d0d48c83f2e54387436e790cb457
Erik O Gabrielsson <erik.o.gabrielsson@sectra.com>
2023-11-05 19:17:18 +0100

    Check if image codecs are available

--- wsidicom/decoder.py
+++ wsidicom/decoder.py
@@ -1,11 +1,12 @@
 import io
 from abc import ABCMeta, abstractmethod
-from typing import Dict, Optional, Type
+from typing import Callable, Dict, Optional, Type
 
 from highdicom.frame import decode_frame
-from imagecodecs.imagecodecs import jpeg2k_decode, jpeg_decode, jpegls_decode
+from imagecodecs import jpeg2k_decode, jpeg_decode, jpegls_decode, JPEG2K, JPEG8, JPEGLS
 from PIL import Image
 from PIL.Image import Image as PILImage
+import numpy as np
 from pydicom import Dataset
 from pydicom import config as pydicom_config
 from pydicom.uid import (
@@ -69,7 +70,7 @@ class Decoder(metaclass=ABCMeta):
         if config.settings.prefered_decoder is not None:
             if config.settings.prefered_decoder not in decoders:
                 raise ValueError(
-                    f"Unknown prefered decoder: {config.settings.prefered_decoder}"
+                    f"Unknown prefered decoder: {config.settings.prefered_decoder}."
                 )
             decoder = decoders[config.settings.prefered_decoder]
             if not decoder.is_supported(transfer_syntax):
@@ -146,34 +147,28 @@ class PydicomDecoder(Decoder):
 
 
 class ImageCodecsDecoder(Decoder):
-    _supported_transfer_syntaxes = [
-        JPEGBaseline8Bit,
-        JPEGExtended12Bit,
-        JPEGLosslessP14,
-        JPEGLosslessSV1,
-        JPEGLSLossless,
-        JPEGLSNearLossless,
-        JPEG2000Lossless,
-        JPEG2000,
-    ]
-    _jpeg_transfer_syntaxes = [
-        JPEGBaseline8Bit,
-        JPEGExtended12Bit,
-        JPEGLosslessP14,
-        JPEGLosslessSV1,
-    ]
-    _jpeg_ls_transfer_syntaxes = [JPEGLSLossless, JPEGLSNearLossless]
-    _jpeg2000_transfer_syntaxes = [JPEG2000Lossless, JPEG2000]
+    _supported_transfer_syntaxes = {
+        "jpeg8": [
+            JPEGBaseline8Bit,
+            JPEGExtended12Bit,
+            JPEGLosslessP14,
+            JPEGLosslessSV1,
+        ],
+        "jpegls": [
+            JPEGLSLossless,
+            JPEGLSNearLossless,
+        ],
+        "jpeg2k": [
+            JPEG2000Lossless,
+            JPEG2000,
+        ],
+    }
 
     def __init__(self, transfer_syntax: UID) -> None:
-        if transfer_syntax in self._jpeg_transfer_syntaxes:
-            self._decoder = jpeg_decode
-        elif transfer_syntax in self._jpeg_ls_transfer_syntaxes:
-            self._decoder = jpegls_decode
-        elif transfer_syntax in self._jpeg2000_transfer_syntaxes:
-            self._decoder = jpeg2k_decode
-        else:
+        decoder = self._get_decoder(transfer_syntax)
+        if decoder is None:
             raise ValueError(f"Unsupported transfer syntax: {transfer_syntax}.")
+        self._decoder = decoder
 
     def decode(self, frame: bytes) -> PILImage:
         decoded = self._decoder(frame)
@@ -181,4 +176,24 @@ class ImageCodecsDecoder(Decoder):
 
     @classmethod
     def is_supported(cls, transfer_syntax: UID) -> bool:
-        return transfer_syntax in cls._supported_transfer_syntaxes
+        decoder = cls._get_decoder(transfer_syntax)
+        return decoder is not None
+
+    @classmethod
+    def _get_decoder(
+        cls, transfer_syntax: UID
+    ) -> Optional[Callable[[bytes], np.ndarray]]:
+        decoder_name = next(
+            (
+                decoder_name
+                for decoder_name, transfer_syntaxes in cls._supported_transfer_syntaxes.items()
+                if transfer_syntax in transfer_syntaxes
+            ),
+            None,
+        )
+        if decoder_name == "jpeg8" and JPEG8.available:
+            return jpeg_decode
+        if decoder_name == "jpegls" and JPEGLS.available:
+            return jpegls_decode
+        if decoder_name == "jpeg2k" and JPEG2K.available:
+            return jpeg2k_decode
