53ce4b95481a7d4dae76a7a8ad3c187c1547a532
Erik O Gabrielsson <erik.o.gabrielsson@sectra.com>
2025-03-13 14:18:58 +0100

    Version 0.25.0

--- wsidicom/__init__.py
+++ wsidicom/__init__.py
@@ -30,7 +30,7 @@ from wsidicom.series import Labels, Overviews, Pyramid
 from wsidicom.web import WsiDicomWebClient
 from wsidicom.wsidicom import WsiDicom
 
-__version__ = "0.24.0"
+__version__ = "0.25.0"
 
 __all__ = [
     "settings",
